df9c8728804d57f2415f58f36d1739568764d178
huaowei <hawgzyx@163.com>
2023-12-20 20:40:45 +0800

    上传目标检测模型

--- TextProcessor.py
+++ TextProcessor.py
@@ -11,24 +11,24 @@ from nltk.tokenize import word_tokenize
 class TextProcessor:
     def __init__(self, video_name):
         self.video_name = video_name
-        self.image_dir = os.path.join('new_yolo_res', video_name)
-        self.label_dir = os.path.join('new_yolo_res', video_name, 'labels')
+        self.image_dir = os.path.join("new_yolo_res", video_name)
+        self.label_dir = os.path.join("new_yolo_res", video_name, "labels")
         self.time_interval = 10
         self.input_dir = os.path.join("result_all_txt", video_name)
         self.output_dir = os.path.join("label_hb_txt", video_name)
-        self.folder_path = os.path.join('result_all_txt', video_name)
-        self.image_files = [f for f in os.listdir(self.image_dir) if f.endswith('.jpg')]
+        self.folder_path = os.path.join("result_all_txt", video_name)
+        self.image_files = [f for f in os.listdir(self.image_dir) if f.endswith(".jpg")]
         self.nums_pic = len(self.image_files)
-        self.label_files = [f for f in os.listdir(self.label_dir) if f.endswith('.txt')]
+        self.label_files = [f for f in os.listdir(self.label_dir) if f.endswith(".txt")]
         self.number_list = []
         self.textL = 0
 
     def find_missing_files(self):
         for img_file in self.image_files:
-            txt_file = img_file[:-4] + '.txt'
+            txt_file = img_file[:-4] + ".txt"
             if txt_file not in self.label_files:
                 file_name = os.path.splitext(img_file)[0]
-                file_name = file_name.split('_')[-1]
+                file_name = file_name.split("_")[-1]
                 if file_name.isdigit() and len(file_name) == 3:
                     self.number_list.append(int(file_name))
                     try:
@@ -67,23 +67,30 @@ class TextProcessor:
 
     def read_text_files(self):
         texts = []
-        txt_files = sorted([file for file in os.listdir(self.folder_path) if file.endswith('.txt')])
+        txt_files = sorted(
+            [file for file in os.listdir(self.folder_path) if file.endswith(".txt")]
+        )
         self.textL = len(txt_files)
         for file in txt_files:
             file_path = os.path.join(self.folder_path, file)
-            with open(file_path, 'r', encoding='utf-8') as f:
+            with open(file_path, "r", encoding="utf-8") as f:
                 text = f.read()
                 text = re.sub(
-                    r'(\d)(?=[\u4e00-\u9fff])|([\u4e00-\u9fff])(?=\d)|([\u4e00-\u9fff])(?=[.,，。!?])|([.,，。!?])(?=[\u4e00-\u9fff])',
-                    r'\1 \2 \3 \4 ', text)
+                    r"(\d)(?=[\u4e00-\u9fff])|([\u4e00-\u9fff])(?=\d)|([\u4e00-\u9fff])(?=[.,，。!?])|([.,，。!?])(?=[\u4e00-\u9fff])",
+                    r"\1 \2 \3 \4 ",
+                    text,
+                )
                 # 定义要使用的分隔符
-                separators = ['的', '与', '和']
+                separators = ["的", "与", "和"]
                 # 用于存储结果的列表
                 result2 = []
                 # 遍历每个字符串
                 words2 = list(jieba.cut(text)) + word_tokenize(text)
-                words2 = [word for word in words2 if
-                          re.match(r'^[\u4e00-\u9fa5a-zA-Z0-9]+$', word) and len(word) > 3] + word_tokenize(text)
+                words2 = [
+                    word
+                    for word in words2
+                    if re.match(r"^[\u4e00-\u9fa5a-zA-Z0-9]+$", word) and len(word) > 3
+                ] + word_tokenize(text)
                 print(words2)
                 for item in words2:
                     # 初始化当前子集
@@ -94,7 +101,7 @@ class TextProcessor:
                         # 如果字符是分隔词，则添加当前子集到结果列表，并重置子集
                         if char in separators:
                             if subset:
-                                result2.append(''.join(subset))
+                                result2.append("".join(subset))
                             subset = []
                         else:
                             # 如果字符不是分隔词，则将字符添加到当前子集中
@@ -102,13 +109,13 @@ class TextProcessor:
 
                     # 添加最后一个子集到结果列表
                     if subset:
-                        result2.append(''.join(subset))
+                        result2.append("".join(subset))
 
                 result2 = set(result2)
 
                 result2 = [item for item in result2 if len(item) >= 2]
                 # print(result2)
-                my_txt = ' '.join(result2)
+                my_txt = " ".join(result2)
                 # print(my_txt)
                 texts.append(my_txt)
         print(texts)
@@ -150,7 +157,7 @@ class TextProcessor:
         files = os.listdir(self.folder_path)
         if len(files) >= xb:
             res_file = files[xb]  # 第2个文件，索引从0开始
-            file_order = int(os.path.splitext(res_file)[0].split('_')[-1])
+            file_order = int(os.path.splitext(res_file)[0].split("_")[-1])
             return file_order - 1
         else:
             print(f"下标错误，目录中没有足够的文件")
@@ -165,7 +172,12 @@ class TextProcessor:
         input_list = new_data
         maxnum = self.nums_pic
         output_list = [(1, input_list[0][0] + 1)]
-        output_list.extend([(input_list[i][0] + 1, input_list[i + 1][0] + 1) for i in range(len(input_list) - 1)])
+        output_list.extend(
+            [
+                (input_list[i][0] + 1, input_list[i + 1][0] + 1)
+                for i in range(len(input_list) - 1)
+            ]
+        )
         output_list.append((input_list[-1][0] + 1, maxnum))
         return output_list
 
@@ -184,22 +196,27 @@ class TextProcessor:
                 file_pattern = f"*_{idx:03d}.txt"
                 matching_files = glob.glob(os.path.join(self.input_dir, file_pattern))
                 for matching_file in matching_files:
-                    with open(matching_file, "r", encoding='utf-8') as file:
+                    with open(matching_file, "r", encoding="utf-8") as file:
                         content = file.read()
                         merged_content.append(content)
 
         output_file_name = f"{start_idx}_{end_idx}.txt"
         output_file_path = os.path.join(self.output_dir, output_file_name)
 
-        with open(output_file_path, "w", encoding='utf8') as output_file:
+        with open(output_file_path, "w", encoding="utf8") as output_file:
             output_file.write("\n".join(merged_content))
 
-        print(f"Merged content for knowledge point ({start_idx}, {end_idx}) written to {output_file_path}")
+        print(
+            f"Merged content for knowledge point ({start_idx}, {end_idx}) written to {output_file_path}"
+        )
 
     def process_knowledge_points(self):
         self.find_missing_files()
-        knowledge_points = self.generate_output_list(self.process_similarity_scores(
-            self.calculate_similarity(self.read_text_files()) ))
+        knowledge_points = self.generate_output_list(
+            self.process_similarity_scores(
+                self.calculate_similarity(self.read_text_files())
+            )
+        )
         # print(knowledge_points)
         start_time = 0
 
@@ -215,11 +232,12 @@ class TextProcessor:
             self.merge_txt_files(start_idx, end_idx)
             strings.append(
                 f"知识点 {idx + 1}: 开始帧图片索引：{start_idx}，结束帧图片索引：{end_idx}，"
-                f"开始时间：{knowledge_start_time}秒，结束时间：{knowledge_end_time}秒.")
+                f"开始时间：{knowledge_start_time}秒，结束时间：{knowledge_end_time}秒."
+            )
         print(strings)
 
 
 if __name__ == "__main__":
-    video_name_arg = sys.argv[1] if len(sys.argv) > 1 else '3.6'
+    video_name_arg = sys.argv[1] if len(sys.argv) > 1 else "3.6"
     text_processor = TextProcessor(video_name_arg)
     text_processor.process_knowledge_points()
--- VideoProcessor.py
+++ VideoProcessor.py
@@ -13,31 +13,39 @@ class VideoProcessor:
         self.input_directory = input_directory
         self.interval_seconds = interval_seconds
         self.video_filename = f"{self.video_name}.mp4"
-        self.result_folder = os.path.join('./result_all_txt', self.video_name)
+        self.result_folder = os.path.join("./result_all_txt", self.video_name)
         os.makedirs(self.result_folder, exist_ok=True)
         os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
         self.ocr = PaddleOCR(use_angle_cls=True, lang="ch")
 
     def clean_filename(self, filename):
-        cleaned_filename = re.sub(r'[^\w\-_.() ]', '', filename)
-        cleaned_filename = unicodedata.normalize('NFKD', cleaned_filename).encode('ascii', 'ignore').decode('utf-8')
-        cleaned_filename = cleaned_filename.replace(' ', '_')
+        cleaned_filename = re.sub(r"[^\w\-_.() ]", "", filename)
+        cleaned_filename = (
+            unicodedata.normalize("NFKD", cleaned_filename)
+            .encode("ascii", "ignore")
+            .decode("utf-8")
+        )
+        cleaned_filename = cleaned_filename.replace(" ", "_")
         return cleaned_filename
 
     def rename_videos(self):
-        mp4_files = [file for file in os.listdir(self.input_directory) if file.endswith(".mp4")]
+        mp4_files = [
+            file for file in os.listdir(self.input_directory) if file.endswith(".mp4")
+        ]
 
         for mp4_file in mp4_files:
             video_file = os.path.join(self.input_directory, mp4_file)
             cleaned_video_name = self.clean_filename(os.path.splitext(mp4_file)[0])
-            new_video_file = os.path.join(self.input_directory, f"{cleaned_video_name}.mp4")
+            new_video_file = os.path.join(
+                self.input_directory, f"{cleaned_video_name}.mp4"
+            )
             os.rename(video_file, new_video_file)
 
     def replace_punctuation_with_space(self, text):
         # 使用正则表达式将标点符号替换为空格
-        new_text = re.sub(r'[^\w\s]', ' ', text)
+        new_text = re.sub(r"[^\w\s]", " ", text)
         # 使用正则表达式在数字和非数字之间插入空格
-        new_text = re.sub(r'(\d+)(\D)', r'\1 \2', new_text)
+        new_text = re.sub(r"(\d+)(\D)", r"\1 \2", new_text)
         return new_text
 
     def extract_frames(self, video_file):
@@ -70,10 +78,10 @@ class VideoProcessor:
                             merged_text += new_txt + "\n"
                             print(new_txt)
 
-                txt_filename = f"{cleaned_video_name}_{image_count:03d}" + '.txt'
+                txt_filename = f"{cleaned_video_name}_{image_count:03d}" + ".txt"
                 txt_filepath = os.path.join(self.result_folder, txt_filename)
 
-                with open(txt_filepath, 'w', encoding='utf-8') as txt_file:
+                with open(txt_filepath, "w", encoding="utf-8") as txt_file:
                     txt_file.write(merged_text)
                 image_count += 1
 
@@ -83,14 +91,17 @@ class VideoProcessor:
 
     def process_all_mp4_files(self):
         self.rename_videos()
-        mp4_files = [file for file in os.listdir(self.input_directory) if file.endswith(".mp4")]
+        mp4_files = [
+            file for file in os.listdir(self.input_directory) if file.endswith(".mp4")
+        ]
 
         for mp4_file in mp4_files:
             if mp4_file == self.video_filename:
                 video_file = os.path.join(self.input_directory, mp4_file)
                 self.extract_frames(video_file)
 
+
 if __name__ == "__main__":
-    video_name = sys.argv[1] if len(sys.argv) > 1 else '3.6'
+    video_name = sys.argv[1] if len(sys.argv) > 1 else "3.6"
     processor = VideoProcessor(video_name)
     processor.process_all_mp4_files()
